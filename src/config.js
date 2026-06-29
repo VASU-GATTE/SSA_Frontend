5bfc28928d99a77bb876cb0d1e2e36e40d0193b4
VASU-GATTE <vasugatte8@gmail.com>
2026-06-30 00:11:33 +0530

    NEW CHANGES

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
 
-const API_URL="http://localhost:8080";
+const API_URL="https://railway.com/project/9fe98b80-3021-483c-bf16-0281094f4003?environmentId=e83adfd3-ddde-4fc6-8ff4-b659baa5d533";
 
 export default API_URL;
